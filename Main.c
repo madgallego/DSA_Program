6eb11eba1764695a720ec538da1b99cead888aa9
madgallego <mharc.gallego@gmail.com>
2024-11-18 19:43:07 +0800

    ADD: C file and Note file for the project

---
+++ Main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int LinearSearch(int arr[], int N, int x)
+{
+    for (int i = 0; i < N; i++)
+        if (arr[i] == x)
+            return i;
+    return -1;
+}
+
+void main (){
+
+    //Main Screen to be constructed
+
+    //Main Menu Screen
+    printf("")
+    while
+
+
+
+
+
+
+
+}
+
+
