9f15628c80d585d4d4f93a8afb26338305e8bcc9
madgallego <mharc.gallego@gmail.com>
2024-11-19 10:06:07 +0800

    FXD: Restructured thefunction declarations and intializatons to use functions multiple times

--- Main.c
+++ Main.c
@@ -15,169 +15,19 @@ Working Search Function
 #include <stdio.h>
 #define MAX 100
 
-//function that creates an array, pass by reference
-void arrMaker(int *arr, int size){
-    int temp;
-    //gets the user input for the array
-    for(int i=0;i<size;i++){
-        printf("Array Value #%d: ", i+1);
-        scanf("%d", &temp);
-        *(arr+i)=temp;
-    }    
-}
 
-//function that prints an array, pass by reference
-void arrPrinter(int *arr, int size){
-    printf("Array Content: ");
-    for(int i=0;i<size;i++){
-        printf("%d ", arr[i]);
-        
-    }
-    printf("\n");
-}
+//utilities
+void arrMaker(int *arr, int size);
+void arrPrinter(int *arr, int size);
 
+//case1
+void Arrays ();
+void LinearDataStructure();
 
-
-
-//FOR CASE 1 Linear Data Structure
-void Arrays(){
-    int arr[MAX]={0};
-    int size;
-    int choice;
-    int key;
-    printf("Let's first create an array\n");
-    printf("What is the size of the array? ");
-    scanf("%d", &size);
-
-    arrMaker(arr, size);
-    
-    printf("\n\nARRAYS\n");
-    printf("\n1) Traverse\n2) Search\n3) Insert\n4) Delete\n5) Sort\n6) Merge\n7) Exit\n\nPlease choose: ");
-    scanf("%d", choice);
-
-    switch (choice)
-    {
-    case 1: //traverse
-            arrPrinter(arr, size);
-        break;
-    case 2: //Insert
-        /* code */
-        break;
-    case 3:// Delete
-        /* code */
-        break;
-    case 4:
-        /* code */
-        break;
-    case 5:
-        /* code */
-        break;
-    case 6:
-        /* code */
-        break;
-    case 7: //exit
-        break;
-    
-    default:
-        break;
-    }
-
-}
-
-void LinearDataStructure (){
-    int choice;
-    printf("\n1) Arrays\n2) Linked list\n3) Stacks\n4) Queus\n5) Exit\n\nPLease Choose: ");
-    scanf("%d", &choice);
-
-    switch (choice)
-    {
-    case 1: //arrays
-        Arrays();
-        break;
-    case 2: //linked list
-        break;
-    case 3://stacks
-        break;
-    case 4: //queues
-        break;
-    case 5: //exit
-        break;
-    default:
-        break;
-    }
-
-
-
-
-
-}
-
-
-
-
-//FOR CASE 5 Searching
-int linearSearch(int *arr, int size, int key){
-
-    for (int i = 0; i < size; i++)
-        if (arr[i] == key)
-            return i;
-    return -1;
-}
-
-int binarySearch(int *arr, int low, int high, int key){
-while (low <= high) {
-        int mid = low + (high - low)/2;
-
-        if (arr[mid] == key)
-            return mid;
-
-        if (arr[mid] < key)
-            low = mid + 1;
-
-        else
-            high = mid - 1;
-    }
-
-    return -1;
-}
-
-void Searching(int *arr, int size){
-    int choice;
-    int key;
-    int position;
-
-    printf("1) Linear Search\n2) Bianry Search\n3)Exit\n");
-    printf("\nPlease choose: ");
-    scanf("%d", &choice);
-
-    switch (choice)
-    {
-    case 1: //Linear
-        printf("\nKey Value to be searched: ");
-        scanf("%d", &key);
-        position = linearSearch(arr, size, key);
-        if (position<0)
-            printf("Value not found!");
-        else printf("Value %d is at index %d\n\n", key, position);
-        break;
-
-    case 2: //Binary
-        printf("\nKey Value to be searched: ");
-        scanf("%d", &key);
-        position = binarySearch(arr, 0,  size-1, key);
-        if (position<0)
-            printf("Value not found!");
-        else printf("Value %d is at index %d\n\n", key, position);
-        break;
-    case 3: //Exit
-        break;
-    default:
-        break;
-    }
-    
-}
-
-
+//case 5 
+int linearSearch(int *arr, int size, int key);
+int binarySearch(int *arr, int low, int high, int key);
+void Searching(int *arr, int size);
 
 void main (){
     int choice;
@@ -248,11 +98,104 @@ void main (){
             break;
         default:printf("\nWrong Input!\n");
             break;
-        }//while loop
+        } //switch case
+
+    }//while loop
+
 
 }//main
 
+//function that creates an array, pass by reference
+void arrMaker(int *arr, int size){
+    int temp;
+    //gets the user input for the array
+    for(int i=0;i<size;i++){
+        printf("Array Value #%d: ", i+1);
+        scanf("%d", &temp);
+        *(arr+i)=temp;
+    }    
+}
 
+//function that prints an array, pass by reference
+void arrPrinter(int *arr, int size){
+    printf("Array Content: ");
+    for(int i=0;i<size;i++){
+        printf("%d ", arr[i]);
+        
+    }
+    printf("\n");
+}
+
+
+
+
+//FOR CASE 1 Linear Data Structure
+void Arrays(){
+    int arr[MAX]={0};
+    int size;
+    int choice;
+    int key;
+    printf("Let's first create an array\n");
+    printf("What is the size of the array? ");
+    scanf("%d", &size);
+
+    arrMaker(arr, size);
+    
+    printf("\n\nARRAYS\n");
+    printf("\n1) Traverse\n2) Search\n3) Insert\n4) Delete\n5) Sort\n6) Merge\n7) Exit\n\nPlease choose: ");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1: //traverse
+            arrPrinter(arr, size);
+        break;
+    case 2: //Searching
+        printf("\n\nSearch through the array\n");
+        Searching(arr,size);
+        break;
+    case 3://Insert
+        /* code */
+        break;
+    case 4:// Delete
+        /* code */
+        break;
+    case 5:
+        /* code */
+        break;
+    case 6:
+        /* code */
+        break;
+    case 7: //exit
+        break;
+    
+    default:
+        break;
+    }
+
+}
+
+void LinearDataStructure (){
+    int choice;
+    printf("\n1) Arrays\n2) Linked list\n3) Stacks\n4) Queus\n5) Exit\n\nPLease Choose: ");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1: //arrays
+        Arrays();
+        break;
+    case 2: //linked list
+        break;
+    case 3://stacks
+        break;
+    case 4: //queues
+        break;
+    case 5: //exit
+        break;
+    default:
+        break;
+    }
 
 
 
@@ -261,3 +204,63 @@ void main (){
 }
 
 
+//FOR CASE 5 Searching
+int linearSearch(int *arr, int size, int key){
+
+    for (int i = 0; i < size; i++)
+        if (arr[i] == key)
+            return i;
+    return -1;
+}//linear search end of block
+
+int binarySearch(int *arr, int low, int high, int key){
+while (low <= high) {
+        int mid = low + (high - low)/2;
+
+        if (arr[mid] == key)
+            return mid;
+
+        if (arr[mid] < key)
+            low = mid + 1;
+
+        else
+            high = mid - 1;
+    }
+
+    return -1;
+}//bionary search end of block
+
+void Searching(int *arr, int size){
+    int choice;
+    int key;
+    int position;
+
+    printf("1) Linear Search\n2) Bianry Search\n3)Exit\n");
+    printf("\nPlease choose: ");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1: //Linear
+        printf("\nKey Value to be searched: ");
+        scanf("%d", &key);
+        position = linearSearch(arr, size, key);
+        if (position<0)
+            printf("Value not found!");
+        else printf("Value %d is at index %d\n\n", key, position);
+        break;
+
+    case 2: //Binary
+        printf("\nKey Value to be searched: ");
+        scanf("%d", &key);
+        position = binarySearch(arr, 0,  size-1, key);
+        if (position<0)
+            printf("Value not found!");
+        else printf("Value %d is at index %d\n\n", key, position);
+        break;
+    case 3: //Exit
+        break;
+    default:
+        break;
+    }
+}//searching end of block
